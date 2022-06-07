9eb2046a1948222c857f75c24524c17333bf785d
estesthebestes <esteshunter22@gmail.com>
2022-06-07 17:45:08 -0500

    oh no! We've got a 404 error

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 }) 
+
+app.get('*', (req, res) => {
+    res.status(404).send(`<h1>404 Page Not Found</h1>`);
+})
 
 app.listen(process.env.PORT)
