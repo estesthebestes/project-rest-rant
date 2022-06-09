6c25b2d3ba436e754a3bba0597e0d731d8671e91
estesthebestes <esteshunter22@gmail.com>
2022-06-09 17:58:16 -0500

    added home page

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
-app.use('/places', require('./controllers/places'));
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-}) 
+    res.render('home')
+  })
+  
 
 app.get('*', (req, res) => {
-    res.status(404).send(`<h1>404 Page Not Found</h1>`);
+  res.send('404 page')
 })
 
 app.listen(process.env.PORT)
