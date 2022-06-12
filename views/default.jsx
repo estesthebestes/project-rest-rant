52248decdd8387d9ce4b4951e659b63400c55cdf
estesthebestes <esteshunter22@gmail.com>
2022-06-12 11:06:56 -0500

    added css styles

--- views/default.jsx
+++ views/default.jsx
@@ -5,13 +5,18 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
                 {html.children}
             </body>
         </html>
     )
-  }
+}
+
+module.exports = Def
+
 
 
 module.exports = Def
