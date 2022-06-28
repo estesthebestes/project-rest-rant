222a9d0dd158df4e45ea8d05750b5ecbb63f01f4
estesthebestes <esteshunter22@gmail.com>
2022-06-28 18:06:54 -0500

    Adding new places

--- controllers/places.js
+++ controllers/places.js
@@ -1,12 +1,10 @@
 const router = require('express').Router()
 
+
 // More code here in a moment
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
-
-
-
 
 router.get('/', (req, res) => {
   let places = [{
@@ -25,6 +23,27 @@ router.get('/', (req, res) => {
     
     res.render('places/index', { places })
 })
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+
+
+
+
 
 module.exports = router
   
